fea535d4d6c0222a1107ce782bf1de8513e4a104
javaquan <javaquan@qq.com>
2024-01-09 14:46:21 +0800

    feat: 调整百度统计配置

--- public/scripts/baidu.js
+++ public/scripts/baidu.js
@@ -6,7 +6,7 @@
 var _hmt = _hmt || [];
 (function () {
     var hm = document.createElement("script");
-    hm.src = "https://hm.baidu.com/hm.js?987634ed52d7731865d40ee1ef150360";
+    hm.src = "https://hm.baidu.com/hm.js?b0653beba4f5f91c2439247023821394";
     var s = document.getElementsByTagName("script")[0];
     s.parentNode.insertBefore(hm, s);
 })();
